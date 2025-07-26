a343b88e5758f05c85a0186ad6ed005c57e06abd
reytz-code <yarikivanov260@gmail.com>
2025-07-26 11:07:03 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+set -e
 
+# Установка системных зависимостей
+apt-get update
+apt-get install -y python3-dev build-essential libssl-dev libffi-dev
+
+# Установка Python-зависимостей
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
