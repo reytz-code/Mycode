f71342f6ab14c4927dc5008939ba82979cedf9fd
reytz-code <yarikivanov260@gmail.com>
2025-07-26 10:25:58 +0300

    Delete Build.sh

--- Build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Установка без сборки из исходников
-pip install --upgrade pip
-pip install aiohttp==3.8.6 --no-binary :none: --only-binary :all:
-pip install -r requirements.txt
-python bot.py
